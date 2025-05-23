5650e2b22b77a1f183a66380e775a04bea71a5cf
Amit Vallabh Keshav <82057873+Darth-Vallabh@users.noreply.github.com>
2025-05-23 16:22:30 +0200

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=["*"],  # or use your GitHub Pages URL for tighter security
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
